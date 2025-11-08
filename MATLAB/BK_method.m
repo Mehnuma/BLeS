92d11b66a75c6fa26ae4a3254e43658e19bd7deb
Mehnuma <73619185+Mehnuma@users.noreply.github.com>
2025-11-07 19:11:39 -0600

    Update BK_method.m

--- MATLAB/BK_method.m
+++ MATLAB/BK_method.m
@@ -31,7 +31,52 @@ else
 end
 IF_hat = n*(org_stat-new_stat);
 
+% Calculate R-hat
+R_hat = zeros(n, 1);
+t = 1;
+for k=0:n-1
+    R_hat(k+1)= (1/n)*sum((IF_hat(t:(n-k))).*IF_hat((t+k):n));
+end
 
+% Calculation of b's
+b = zeros(5,1);
+b(1) = 1/n;
+% numerator = 2*sum(R_hat.^2)-((R_hat(1)).^2);
+numerator = (R_hat(1))^2 +2*sum((R_hat(2:end).^2));
+denom1 = zeros(n,1);
+for i = 2:5
+    for k = 0:(n-1)
+        wsc_val = wSC(k.*b(i-1).*n^(4/21));
+        denom1(k+1) = (wsc_val).^2.*k^2.*(R_hat(k+1)).^2;
+    end
+    Denominator = 6*2*sum(denom1)+1e-3;
+    b(i) = n^(-1/3)*(numerator/Denominator)^(1/3);
+end
+
+% Calculation of l-hat
+num = zeros(n,1);
+den = zeros(n,1);
+for k = 0:(n-1)
+    wth_val = wTH(k*b(5)*n^(4/21));
+    wsc_val = wSC(k*b(5)*n^(4/21));
+    
+    num(k+1) = wth_val*R_hat(k+1); 
+    den(k+1) = wsc_val*k*R_hat(k+1);
+end
+big_num = 2*((2*sum(num(2:end))+num(1))^2);
+if (sum(den))^2 == 0
+    big_den = 1e-3;
+else
+    big_den = 3*(2*(sum(den))^2);
+end
+b_hat = n^(-1/3)*(big_num/big_den)^(1/3);
+
+if (1/b_hat) < 0.5
+    l_hat = ceil(1/b_hat);
+else
+    l_hat = round(1/b_hat);
+end
+end
 
 %% Window Functions
 function w1 = wTH(x)
