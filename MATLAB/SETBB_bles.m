a4172a5d472ac6f074cff53304bffd4941d54676
Mehnuma <73619185+Mehnuma@users.noreply.github.com>
2025-11-17 11:18:54 -0600

    Update SETBB_bles.m

--- MATLAB/SETBB_bles.m
+++ MATLAB/SETBB_bles.m
@@ -2,14 +2,12 @@ l_hat_setbb = SETBB_bles(X,Y,statistic,estimator_type,quantile)
 % This function implements block length selection for the smooth extended tapered block bootstrap (SETBB) method, 
 % provided by Gregory et al. (2018).
 % Input:
-%        (1) ts- The time series/correlated series of observations 
-%        (2) l_init- Initial block length for the moving block bootstrap (MBB) estimator
-%        (3) m- Subsample size
-%        (4) statistic- Statistic calculated from the data (mean/median)
-%        (5) estimator_type- Bootstrapped functional
-%        (6) B- Number of bootstrap replications
-%        (7) theta_hat-  "True" value used for the bias and distribution estimation
-%        (8) threshold- Cut-off point needed for the distribution estimation
-%        (9) n_iter- Number of iterations for the HHJ algorithm
+%        (1) X- Input in the data pair
+%        (2) Y- Output in the data pair
+%        (3) statistic- Statistic calculated from the data ( for modified SETBB is a quantile regression estimator, 
+%                       and for the regular SETBB, sample quantiles or other smooth estimators)
+%        (4) estimator_type- Bootstrapped functional (for extended SETBB is ‘distribution’, 
+%                                                     and for regular SETBB are ‘variance’ and ‘distribution’)
+%        (5) quantile- The quantile of interest for quantile regression estimator
 % Output:
 %        (1) opt_l- Optimal block length selected by the HHJ method
