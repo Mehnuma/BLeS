2d76659f85972db63985cde169c958df8b21bb10
Mehnuma <73619185+Mehnuma@users.noreply.github.com>
2025-12-12 14:32:15 -0600

    Update TBB_bles.m

--- MATLAB/TBB_bles.m
+++ MATLAB/TBB_bles.m
@@ -1,4 +1,4 @@
-l_hat = TBB_bles(ts,statistic,estimator_type,method)
+function l_hat = TBB_bles(ts,statistic,estimator_type,method)
 % This function selects the optimal block length for the tapered block bootstrap (TBB), proposed by Paparoditis and Politis (2001,2002), 
 % and the extended tapered block bootstrap (ETBBB) by Shao (2010).
 % Input:
@@ -8,3 +8,6 @@ l_hat = TBB_bles(ts,statistic,estimator_type,method)
 %        (4) method- TBB or ETBB (Options: ‘regular’ and ‘extended’)
 % Output:
 %        (1) l_hat- Optimal block length selected for the TBB or ETBB method
+
+
+end
